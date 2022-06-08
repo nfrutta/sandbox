cf1c4fd5b26a419b1b39d6f3481780dcfdfa72fd
Naoyuki Furuta <nfrutta1208@gmail.com>
2022-06-08 14:07:54 +0900

    一旦リセット

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sandbox/second_page.dart';
 
 class FirstPage extends StatelessWidget {
   FirstPage({Key? key}) : super(key: key);
@@ -13,44 +12,7 @@ class FirstPage extends StatelessWidget {
         title: const Text('Flutter Sandbox'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.network(
-                height: 200,
-                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 48.0),
-              child: TextField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'name',
-                ),
-                onChanged: (text) {
-                  nameText = text;
-                },
-              ),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SecondPage(nameText),
-                  ),
-                );
-              },
-              child: const Text('次の画面へ'),
-            ),
-          ],
-        ),
-      ),
+      body: Container(),
     );
   }
 }
